dba55bd14df3c09699408f841fb76d6f50ad373f
SlothfulAlfred <75184429+SlothfulAlfred@users.noreply.github.com>
2020-11-28 17:44:24 -0500

    Adding my current progress
    So far I have defined classes to contain the data and created a .txt file to store the data.
    Format when inputting data in the .txt file should be "name\n" for countries and "name(string), emissions(float), latitude(float), longitude(float)\n" for regions. Make sure to end with a '\n' after each country's list of regions

---
+++ main.py
@@ -0,0 +1,18 @@
+#-----------------------------------------------------
+# Name:             Air Pollution App (main.py)
+# Purpose:          To create an app to raise awareness about climate change and air pollution in North America
+#
+#
+#
+# Author:           Alfred Mikhael, 627582
+# Created:          27-Sep-2020
+# Updated:          27-Sep-2020
+#----------------------------------------------------
+import os
+import sys
+sys.path.append("D:\\Visual Studio Projects\\Projects\\AirPollutionCS\\Classes")
+import country
+import region
+filename = "data.txt"
+
+
---
+++ region.py
@@ -0,0 +1,94 @@
+class Region():
+    """
+    An object that contains the air pollution data and the geocoordinates of a certain area / region
+
+    Attributes
+    -----------------------------
+    name: string
+        The name of the region
+    emissions: float
+        Greenhouse gas emissions in 2018 measured in megatonnes of CO2
+    lat: float
+        Latitudinal coordinates of region
+    lon: float
+        Longitudinal coordinates of region
+
+    Methods
+    ---------------------------
+    getEmissions() -> float
+        returns the greenhouse gas emissions of the region in 2018
+    getCoordinates() -> tuple
+        returns the latitude and longitude of the region as a tuple
+    getName() -> string
+        returns the name of the region
+    
+    """
+
+    def __init__(self, name, em = 0.00, lat = -82.8628, lon = 135.0000):
+        '''
+        Constructor to initialize a region object
+
+        Parameters
+        -----------------------
+        name: string
+            The name of the region
+
+        em: float
+            the annual greenhouse emissions of a region in megatonnes of CO2
+            defaults to zero if nothing is input
+
+        lat: float
+            the latitudinal coordinates of a region
+            defautls to the latitude of Antarctica
+
+        lon: float
+            the longitudinal coordinates of a region
+            defaults to the longitude of Antarctica
+
+        Raises
+        ----------------------
+        ValueError
+            If the emissions are below zero 
+        '''
+        if em < 0.00:
+            raise ValueError("Cannot have negative emissions")
+        self.emissions = em
+        self.lat = lat
+        self.lon = lon
+        self.name = name
+
+
+    def getEmissions(self) -> float:
+        '''
+        returns yearly greenhouse gas emissions
+        
+        Returns
+        ----------------------
+        float:
+            yearly greenhouse gas emissions
+        '''
+        return self.emissions
+
+
+    def getCoordinates(self) -> tuple:
+        '''
+        returns the coordinates of the region
+
+        Returns
+        -----------------------
+        tuple:
+            latitude and longitude coordinates of the region
+        '''
+        return tuple(self.lat, self.lon)
+
+    def getName(self) -> str:
+        '''
+        returns the name of the region
+
+        Returns 
+        ----------------------------
+        string:
+            the name of the region
+        '''
+        return self.name
+
