ac27846bf72080b8d2a5102a5074a76934d2d7b4
johannzhao <43770097+johannzhao@users.noreply.github.com>
2020-12-20 14:22:45 -0500

    Update newgui.py

--- newgui.py
+++ newgui.py
@@ -73,6 +73,31 @@ map_image.create_image(150,150,image = image2,anchor = CENTER)
 map_image.create_rectangle(0,0,300,300, fill = 'grey', stipple = 'gray50')
 map_image.create_text(150,150, width = 300, text = "Map: View the amount of pollution in individual states/provinces.")
 
+#if selected map frame
+CountryFrame = Frame(MapFrame, width=1400, height=800, bg = 'purple')
+CanadaFrame = Frame(MapFrame, width=500, height=500, bg = 'red')
+USAFrame = Frame(MapFrame, width=500, height=500, bg = 'brown')
+MexicoFrame = Frame(MapFrame, width=500, height=500, bg='aqua')
+
+def click():
+
+    if number == 1:
+        CountryFrame.lower
+        CanadaFrame.lift()
+    elif number == 2:
+        CountryFrame.lower       
+        USAFrame.lift()
+    elif number == 3:
+        CountryFrame.lower
+        MexicoFrame.lift()
+
+
+
+CanadaButton = Button(CountryFrame, text = "Canada image here" , command = click(1) )
+USAButton = Button(CountryFrame, text = "USA image here"  , command = click(2))
+MexicoButton = Button(CountryFrame, text = "Mexico image here", command = click(3))
+
+
 # Load all objects
 
 GraphButton.place(anchor = NE, x = 1, y = 1, relx = 1, relwidth = 0.3333333333333333333333)
@@ -81,6 +106,20 @@ HomeButton.place(anchor = NW, x = 1, y = 1, relwidth = 0.3333333333333333333333)
 title_image.pack()
 map_image.pack()
 
+#placing all the frames and defining their size
+MexicoFrame.place(x = 0, y =0)
+USAFrame.place(x = 0, y =0)
+CanadaFrame.place(x = 0, y =0)
+
+#placed last so it will show first
+CountryFrame.place(x=0, y=0)
+CountryFrame.lift()
+
+
+#button placements
+MexicoButton.place(x=400, y =30)
+CanadaButton.place(x=100, y =30)
+USAButton.place(x=250, y =30)
 # Set size of window
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.mainloop()
