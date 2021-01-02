9e4aa29215785a0302590a4d346ad91f73e02755
SlothfulAlfred <75184429+SlothfulAlfred@users.noreply.github.com>
2021-01-02 12:47:53 -0500

    Added test for multiple initializations

--- tests/test_country.py
+++ tests/test_country.py
@@ -1,5 +1,5 @@
 import unittest
-import country
+from Classes.country import Country
 
 
 class Test_Country(unittest.TestCase):
@@ -9,8 +9,14 @@ class Test_Country(unittest.TestCase):
         inp.write("Region One, 1.00, 2.00, 3.00\n" + "Region Two, 199.99, 44.88, 153.888\n")
         inp.write('\n')
         inp.close()
-        global cntry 
-        cntry = country.Country("input.txt")
+        inp_two = open('input_two.txt', 'w')
+        inp_two.write("Country Two\n")
+        inp_two.write("Region Three, 2.33, 3.44, 35.5\n" + "Region Four, 355.5, 89.9, 99.99\n")
+        inp_two.write('\n')
+        inp_two.close()
+        global cntry, cntry_two 
+        cntry = Country("input.txt")
+        cntry_two = Country('input_two.txt')
 
     def tearDown(self):
         pass
@@ -23,6 +29,11 @@ class Test_Country(unittest.TestCase):
         self.assertEqual("Country One", cntry.getName())
         self.assertEqual(200.99, cntry.getEmissions())
         self.assertEqual(["Region One", "Region Two"], [x for x in cntry.getRegions()])
+
+    def test_country_multiple_initialization(self):
+        self.assertEqual(cntry_two.name, "Country Two")
+        self.assertEqual(len(cntry_two.regions), 2)
+
 
 if __name__ == "__main__":
     unittest.main()
