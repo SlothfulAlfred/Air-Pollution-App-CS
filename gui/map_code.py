28ff56bfe8258d00dc687df14c0b9b5f5f9dcaba
Rishbob <43894878+Rishbob@users.noreply.github.com>
2021-01-04 13:33:11 -0500

    Image creation loop added
    Added in a loop that automatically generates the images, depending on what images are available. We also noticed that the online map generation was what was causing the previously noticed delays, so it has been removed for now.

--- gui/map_code.py
+++ gui/map_code.py
@@ -1,41 +1,37 @@
 import plotly.express as px
 import sys
 import os
-import datetime
 
-print(datetime.datetime.now())
-print(os.getcwd())
 path_parent = os.path.dirname(os.getcwd())
 os.chdir(path_parent)
-print(os.getcwd())
-# do you need sys and os? also why are you printing the cwd
+
+
+data_pathlist = ["map\\docs\\canada.txt","map\\docs\\usa.txt","map\\docs\\mexico.txt"]
+image_pathlist = ["map/images/canada_map.png","map/images/usa_map.png","map/images/mexico_map.png"]
 
 from source.helpers.region import Region
 from source.helpers.country import Country
-data = open(r"map\\docs\\usa.txt")
 
-# you don't need to use a kwarg here since file is the only argument that is needed
-Canada = Country(file = "map\\docs\\usa.txt") 
-canada_size = len(Canada.regions)
-# you don't need to declare these first you can do everything during the list comprehension 
-lat_list = []
-lon_list = []
-em_list = []
+i = 0
+while i in range(len(data_pathlist)):
+    if os.path.isfile(image_pathlist[i]) == False:
+        data = open(data_pathlist[i], 'r')
+        Map = Country(file = data_pathlist[i])
+        map_size = len(Map.regions)
+        lat_list = []
+        lon_list = []
+        em_list = []
 
-i = 0 # this variable is not necessary 
-# why not iterate through the list directly instead of iterating through a range of subscripts then accessing the list
-# i.e [region.lat for region in Canada.regions]
-lat_list = [(Canada.regions[i].lat) for i in range (canada_size)] 
-lon_list = [(Canada.regions[i].lon) for i in range (canada_size)]
-em_list = [(Canada.regions[i].emissions) for i in range (canada_size)]
+        lat_list = [(Map.regions[i].lat) for i in range (map_size)]
+        lon_list = [(Map.regions[i].lon) for i in range (map_size)]
+        em_list = [(Map.regions[i].emissions) for i in range (map_size)]
 
-df = em_list
+        df = em_list
 
-fig = px.scatter_mapbox(df, lat=lat_list, lon=lon_list,
-                        color = em_list, color_continuous_scale=px.colors.cyclical.IceFire,
+        fig = px.scatter_mapbox(df, lat=lat_list, lon=lon_list,
+                        color = em_list, color_continuous_scale=px.colors.sequential.Viridis,
                         size_max=30, zoom=1.5)
-fig.update_layout(mapbox_style="carto-positron")
-fig.show()
-fig.write_image("map/images/usa_map.png")
-fig.to_image(format="png", engine="kaleido")
-print("Done at",datetime.datetime.now())
+        fig.update_layout(mapbox_style="carto-positron")
+        fig.write_image(image_pathlist[i])
+        fig.to_image(format="png", engine="kaleido")
+    i += 1
