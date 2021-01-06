39577663dfa00193e23def0a370bbc4c0672f861
Rishbob <43894878+Rishbob@users.noreply.github.com>
2021-01-06 13:09:15 -0500

    Implemented feedback from Alfred
    Altered program to take a Country object as input, rather than a string.

--- source/helpers/country_map_generator.py
+++ source/helpers/country_map_generator.py
@@ -18,39 +18,26 @@ import os
 from region import Region
 from country import Country
 
-#Function (country)
 
-# Please alter this to take a Country object as a parameter instead of a name. I believe that we already talked about
-# this during the last discord meeting.
-def region_map(name):
-    
-    # What is the point of this variable.
-    i = 0
-    
+def region_map(country):
+    name = country.name
     name = name.lower()
     map_pathname = ("docs\\" + name + ".txt")
-    # why would you save the map in the maps folder instead of in the images folder. 
-    image_pathname = ("source/helpers/maps/" + name + "_map.png")
-    
-    if os.path.isfile(image_pathname) == False:
-        
-        data = open(map_pathname, 'r')
-        Map = Country(file = map_pathname)
-        map_size = len(Map.regions)
-        
-        # .................
-        # What happened to the list comprehension that you had before?
-        # this is way harder to read and it's slower. 
-        lat_list = [(Map.regions[i].lat) for i in range (map_size)]
-        lon_list = [(Map.regions[i].lon) for i in range (map_size)]
-        em_list = [(Map.regions[i].emissions) for i in range (map_size)]
+    image_pathname = ("source/helpers/images/" + name + "_map.png")
 
-        fig = px.scatter_mapbox(data_file = em_list, lat=lat_list, lon=lon_list,
-                        color = em_list, color_continuous_scale=px.colors.sequential.Viridis,
-                        size_max=30, zoom=1.5)
-        
+    if os.path.isfile(image_pathname) == False:
+        data = open(map_pathname, 'r')
+        Map = Country(file=map_pathname)
+        map_size = len(Map.regions)
+
+        lat_list = [x.lat for x in Map.regions]
+        lon_list = [x.lon for x in Map.regions]
+        em_list = [x.emissions for x in Map.regions]
+
+        fig = px.scatter_mapbox(data_frame=em_list, lat=lat_list, lon=lon_list,
+                                color=em_list, color_continuous_scale=px.colors.sequential.Viridis,
+                                size_max=30, zoom=1.5)
+
         fig.update_layout(mapbox_style="carto-positron")
         fig.write_image(image_pathname)
         fig.to_image(format="png", engine="kaleido")
-    # What is the point of this variable i?
-    i += 1
