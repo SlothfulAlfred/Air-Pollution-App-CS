42140ba1133b0c0099e9a9f15a003bf7cb352de5
Rishbob <43894878+Rishbob@users.noreply.github.com>
2021-01-07 17:12:16 -0500

    Update continent_map_generator.py
    Program now can theoretically scale infinitely, and is now streamlined with list comprehension.

--- source/helpers/continent_map_generator.py
+++ source/helpers/continent_map_generator.py
@@ -9,8 +9,8 @@
 #				(the -> notation).
 #
 # Author:      Rishabh Tamhane
-# Created:     13-Jan-2021
-# Updated:     13-Jan-2021
+# Created:     06-Jan-2021
+# Updated:     07-Jan-2021
 #-----------------------------------------------------------------------------
 
 import plotly.express as px
@@ -18,52 +18,41 @@ import os
 from region import Region
 from country import Country
 
-# I strongly strongly strongly strongly suggest that you just take a list of Country
-# objects here. You should write your code with the mentality that it might need
-# to be updated and/or maintained in the future so make it easy for yourself.
-
-# In the PEP-8 standards, inline comments need to be separated from the code by 
-# at least 2 indentations (8 spaces). Also you should generally avoid them and 
-# opt for block comments which are like what I'm writing now. These are on a 
-# separate line and refer to all/most of the code that is on the same indentation as 
-# them or in the following block. 
-
 def continent_map():
     '''
     Generates a Scatterbox map of North America, displaying the emissions data for each country.
     '''
+    countries = os.listdir('docs')          #List of pathways to countries' data
+    i=0
+    for i in range(len(countries)):
+        countries[i] = str("docs/" + countries[i])          #Appends "docs/" to each element of the list (ensures correct pathways are found)
+
 
     #Data Order for All Lists: Canada, USA, Mexico
-    data_pathname = ["docs\canada.txt","docs\\usa.txt","docs\\mexico.txt"] #List of pathways to countries' data
-    image_pathname = ("source/helpers/images/continent_map.png") #List of pathway for the final map
+    image_pathname = ("source/gui/images/continent_map.png")        #List of pathway for the final map
 
-    # You should use the new built-in lat and lon attributes in the 
-    # Country class. I used these values to create it so the map
-    # won't change much.
-    #
-    # filling out emissions list would be better using list comprehension. 
-    
-    lat_list = [49.6937, 39.8283, 19.432608] #List of latitudes of the countries' geographic centres
-    lon_list = [-96.8441, -98.5795, -99.133209] #List of longitudes of the countries' geographic centres
-    em_list = [] #List of emissions of CO2 (Mt, 2018) of each country
+    country_list = []        #List of all countries (contains Country objects)
+    lon_list = []         #List of longitudes of the countries' geographic centres
+    em_list = []        #List of emissions of CO2 (Mt, 2018) of each country
 
-    i = 0
-    if os.path.isfile(image_pathname) == False: #Checks if the map has already been generated
-        while i <= 2:
-            
-            open(data_pathname[i], 'r') 
-            Map = Country(file=data_pathname[i]) #Loads up a specific country, reads data from data_pathname
-            emissions = Map.getEmissions() #getEmissions function gets the total emissions of a country
-            em_list.append(emissions) #Appends emissions value of country to em_list
-
+    i = 1
+    if os.path.isfile(image_pathname) == False:       #Checks if the map has already been generated
+        while i < len(countries):
+            open(countries[i], 'r')         #Loads up a specific country, reads data from countries entry
+            Map = Country(file=countries[i])
+            country_list.append(Map)        #Adds Country object to country_list
             i = i + 1
-            
-        fig = px.scatter_mapbox(data_frame=em_list, lat=lat_list, lon=lon_list, #Generates map of continent
+
+        #Save emissions, latitude and longitude data to seperate arrays
+        em_list = [country_list[x].getEmissions() for x in range(len(country_list))]        #getEmissions function gets the total emissions of a country
+        lat_list = [country_list[x].lat for x in range(len(country_list))]
+        lon_list = [country_list[x].lon for x in range(len(country_list))]
+
+        #Generates map
+        fig = px.scatter_mapbox(data_frame=em_list, lat=lat_list, lon=lon_list,
                                 color=em_list, color_continuous_scale=px.colors.sequential.Bluered,
                                 color_continuous_midpoint = 700, range_color = (0,1700),
-                                size_max=100, zoom=1.5,
-                                )
-
-        fig.update_layout(mapbox_style="carto-positron") #Sets map style
+                                size_max=100, zoom=1.5)
+        fig.update_layout(mapbox_style="carto-positron")        #Sets map style
         fig.to_image(format="png", engine="kaleido")
-        fig.write_image(image_pathname) #Exports map
+        fig.write_image(image_pathname)         #Exports map
