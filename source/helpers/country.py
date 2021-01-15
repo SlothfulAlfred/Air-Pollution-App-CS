8fc9243d477fa23e885a92a3cf06330a8ecbab12
SlothfulAlfred <75184429+SlothfulAlfred@users.noreply.github.com>
2021-01-15 17:51:19 -0500

    doc: removed comments
    the getName() and getCoordinates() functions are just normal get functions that pretty much every class has. Also, I believe get coordinates is called in some of my other code.

--- source/helpers/country.py
+++ source/helpers/country.py
@@ -21,11 +21,11 @@ class Country():
     -------------------------------
     getEmissions() -> float
         Returns the sum of the emissions of the affiliated regions
-    getName() -> str #Why is this a function, it simply calls a single attribute that can already be obtained with OBJECTNAME.name().
+    getName() -> str 
         Returns the name of the country 
     getRegions() -> list
         Returns the names of the affiliated regions as a list
-    getCoordinates() -> tuple #Feels uneccessary, len and lon are already attributes that can be called. Additionally, the instances where a program would need a tuple as an input seems rare, so I dont see why you chose it specifically.
+    getCoordinates() -> tuple
         Returns a tuple of the coordinates 
 
     '''
